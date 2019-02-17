03889d59ffc517ee4997e00d6204e060c085d479
Jalp <jalpdesai@icloud.com>
2019-02-17 13:37:25 +0000

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import com.jfoenix.controls.JFXButton;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+        primaryStage.setTitle("Hello World");
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("customstyle.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        SQLShenanigans sqlShenanigans = new SQLShenanigans();
+
+    }
+}
