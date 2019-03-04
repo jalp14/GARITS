089dae4bb9718f7d97fc9a0306587a7cdf591e39
Jalp <jalpdesai@icloud.com>
2019-03-04 23:25:11 +0000

    Navigation Logic cleanup and some work done on multi-user support (almost done)

---
+++ src/TwentyThreeProductions/Controller/MainMechanicController.java
@@ -0,0 +1,77 @@
+package TwentyThreeProductions.Controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainMechanicController {
+
+    @FXML
+    private JFXButton jobsBtn;
+
+    @FXML
+    private JFXButton customerBtn;
+
+    @FXML
+    private JFXButton partsBtn;
+
+    @FXML
+    private JFXButton reportsBtn;
+
+    @FXML
+    private JFXButton usersBtn;
+
+    @FXML
+    private JFXButton backuprestoreBtn;
+
+    @FXML
+    private JFXButton notifsBtn;
+
+    @FXML
+    private JFXButton logoutBtn;
+
+    @FXML
+    void backuprestoreBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void jobsBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutBtnPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void notifsBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void partsBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportsBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usersClicked(ActionEvent event) {
+
+    }
+
+    public void initialize() {
+        System.out.println("Init Mechanic Controller");
+    }
+
+}
